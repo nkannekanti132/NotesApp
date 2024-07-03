926e2f5170758104506d33f6982042bed7b5712e
nkannekanti771 <150879728+nkannekanti771@users.noreply.github.com>
2024-07-03 16:30:23 +0530

    Notes App

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.notesapp.database.NoteDatabase
+import com.example.notesapp.repository.NoteRepository
+import com.example.notesapp.viewmodel.NoteViewModel
+import com.example.notesapp.viewmodel.NoteViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+    lateinit var noteViewModel: NoteViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel()
+    {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val noteViewModelFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel =ViewModelProvider(this, noteViewModelFactory)[NoteViewModel::class.java]
+    }
+}
---
+++ app/src/main/java/com/example/notesapp/repository/NoteRepository.kt
@@ -0,0 +1,20 @@
+package com.example.notesapp.repository
+
+import androidx.lifecycle.LiveData
+import com.example.notesapp.database.NoteDatabase
+import com.example.notesapp.model.Note
+
+class NoteRepository(private val db:NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDAO().insertNote(note)
+
+    suspend fun deleteNote(note: Note) = db.getNoteDAO().deleteNote(note)
+
+    suspend fun updateNote(note: Note) = db.getNoteDAO().updateNote(note)
+
+    fun getNotes():LiveData<List<Note>> = db.getNoteDAO().getAllNotes()
+
+    fun searchNotes(query: String?):LiveData<List<Note>> = db.getNoteDAO().searchNote(query)
+
+
+}
